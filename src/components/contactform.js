fe9e90b5e6c4c0656be999d12ac18ee5928c58e8
Prashanth Moorthy <chapparfellow@gmail.com>
2021-05-08 21:23:20 +0530

    More validation

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,171 +1,173 @@
 import React, { Fragment } from "react";
 import { Field, reduxForm } from "redux-form";
-import { Form, Message, Input, Divider } from "semantic-ui-react";
-
-const email = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-  'Invalid email address' : undefined
+import { Icon, Form, Message, Input, Divider } from "semantic-ui-react";
 
 
-const renderCheckbox = field => (
-  <Form.Checkbox
-    checked={!!field.input.value}
-    name={field.input.name}
-    label={field.label}
-    onChange={(e, { checked }) => field.input.onChange(checked)}
-  />
-);
+const validate = values => {
+  const errors = {}
 
-const renderRadio = field => (
-  <Form.Radio
-    checked={field.input.value === field.radioValue}
-    label={field.label}
-    name={field.input.name}
-    onChange={(e, { checked }) => field.input.onChange(field.radioValue)}
-  />
-);
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    return errors
+}
+const email = (value) =>
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+        ? "Invalid email address"
+        : undefined;
+const phone = (value) =>
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+        ? "Invalid email address"
+        : undefined;
 
-const renderSelect = field => (
-  <Form.Select
-    label={field.label}
-    name={field.input.name}
-    onChange={(e, { value }) => field.input.onChange(value)}
-    options={field.options}
-    placeholder={field.placeholder}
-    value={field.input.value}
-  />
-);
-
-const renderField = field => (
-        <Form.Field
-      id='form-input-control-error-email'
-      control={Input}
-      label='Email'
-      placeholder='joe@schmoe.com'
-      error={field.validate ? null : { 
-        content: 'Please enter a valid email address',
-        pointing: 'below',
-      }}
+const renderSelect = (field) => (
+    <Form.Select
+        label={field.label}
+        name={field.input.name}
+        onChange={(e, { value }) => field.input.onChange(value)}
+        options={field.options}
+        placeholder={field.placeholder}
+        value={field.input.value}
     />
-)
-
-const renderTextArea = field => (
-  <Form.TextArea
-    {...field.input}
-    label={field.label}
-    placeholder={field.placeholder}
-  />
 );
 
-const ProfileForm = props => {
-  const { handleSubmit, reset } = props;
-
-  return (
-    <Fragment>
-      <Message info>
-        <p>
-          You will don't need any special mappings for <code>Form.Input</code>,
-          because it passed events from native inputs.
-        </p>
-
-        <p>
-          The situation with other components is more complicated, because the{" "}
-          <code>Field</code> relies on the native events. However, it can be
-          easily with{" "}
-          <a
-            href="https://redux-form.com/7.4.2/docs/api/field.md/#2-a-stateless-function"
-            target="_blank"
-          >
-            stateless function
-          </a>. We recomend to wrap them with generic components to reduce forms
-          complexivity.
-        </p>
-      </Message>
-
-      <Form onSubmit={handleSubmit}>
-        <Form.Group widths="equal">
-          <Field
-            component={Form.Input}
-            label="First name"
-            name="firstName"
-            placeholder="First name"
-          />
-          <Field
-            component={Form.Input}
-            label="Last name"
-            name="lastName"
-            placeholder="Last name"
-          />
-        </Form.Group>
-
-    <Divider hidden />
-
-
-        <Form.Group widths="equal">
-          <Field
-            component={renderSelect}
-            label="Age Group"
-            name="agegroup"
-            options={[
-              { key: "m", text: "18 - 45 Years", value: "18" },
-              { key: "f", text: "45+ Years", value: "45" }
-            ]}
-            placeholder="Age Group"
-          />
-          <Field
-            component={renderSelect}
-            label="Vaccine Choice"
-            name="vaccine"
-            options={[
-              { key: "ANY", text: "Any", value: "ANY" },
-              { key: "COVI", text: "COVISHEILD", value: "COVI" },
-              { key: "COVAX", text: "COVAXIN", value: "COVAX" }
-            ]}
-            placeholder="Select your Vaccine"
-          />
-        </Form.Group>
-
-      <Divider hidden />
-        <Form.Group widths="equal">
-      <Field name="email" 
-            type="email"
-            component={renderField} 
+const renderField = (field) => (
+    <Form>
+        <Input
             label="Email"
-            validate={email}
-      />
-      
-      <Field name="phone" 
-            type="phone"
-            component={renderField} 
-            label="Phone"
-            validate={email}
-      />
-        </Form.Group>
-      
-      <Divider hidden />
+            placeholder="joe@schmoe.com"
+            icon="at"
+            iconPosition="left"
+        />
+    </Form>
+    /*
+    <Input iconPosition="left" placeholder="Email">
+        <Icon name="at" />
+        <input />
+    </Input>
 
-        <Form.Group inline>
-          <Form.Button primary>Submit</Form.Button>
-          <Form.Button onClick={reset}>Reset</Form.Button>
-        </Form.Group>
-      </Form>
-
-    <Message
-      success
-      header='Form Completed'
-      content="You're signed up for Vaccination appointment alerts. We will send you an email when a vaccination slot is available."
+    <Form.Field
+        name={field.input.name}
+        control={Input}
+        label={field.label}
+        placeholder={field.placeholder}
     />
+*/
+);
 
-    <Message
-      error
-      header='Registration Failed'
-      content='The email you entered is already regsitered.'
-    />
+const ProfileForm = (props) => {
+    const { handleSubmit, reset } = props;
 
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group widths="equal">
+                    <Field
+                        component={Form.Input}
+                        label="First name"
+                        name="firstName"
+                        placeholder="First name"
+                        required={true}
+                    />
+                    <Field
+                        component={Form.Input}
+                        label="Last name"
+                        name="lastName"
+                        placeholder="Last name"
+                    />
+                </Form.Group>
+
+                <Divider hidden />
+
+                <Form.Group widths="equal">
+                    <Field
+                        component={renderSelect}
+                        label="Age Group"
+                        name="ageGroup"
+                        options={[
+                            { key: "m", text: "18 - 45 Years", value: "18" },
+                            { key: "f", text: "45+ Years", value: "45" },
+                        ]}
+                        placeholder="Age Group"
+                        required={true}
+                    />
+                    <Field
+                        component={renderSelect}
+                        label="Vaccine Choice"
+                        name="vaccine"
+                        options={[
+                            { key: "ANY", text: "Any", value: "ANY" },
+                            { key: "COVI", text: "COVISHEILD", value: "COVI" },
+                            { key: "COVAX", text: "COVAXIN", value: "COVAX" },
+                        ]}
+                        placeholder="Select your Vaccine"
+                        required={true}
+                    />
+                    <Field
+                        component={renderSelect}
+                        label="Price"
+                        name="price"
+                        options={[
+                            { key: "ANY", text: "Any", value: "ANY" },
+                            { key: "FREE", text: "Free", value: "FREE" },
+                            { key: "PAID", text: "Paid", value: "PAID" },
+                        ]}
+                        placeholder="Price"
+                        required={true}
+                    />
+                </Form.Group>
+
+                <Divider hidden />
+                <Form.Group widths="equal">
+                    <Field
+                        component={Form.Input}
+                        label="Email"
+                        name="email"
+                        type="email"
+                        icon = "at"
+                        iconPosition= "left"
+                        placeholder="john.doe@gmail.com"
+                        required={true}                 
+        validate={email}
+                    />
+                    <Field
+                        component={Form.Input}
+                        label="Phone"
+                        name="phone"
+                        type="phone"
+                        icon = "phone"
+                        iconPosition= "left"
+                        placeholder="10-digit phone number"
+                        //validate={phone}
+                    />
+                </Form.Group>
+
+                <Divider hidden />
+
+                <Form.Group inline>
+                    <Form.Button primary>Submit</Form.Button>
+                    <Form.Button onClick={reset}>Reset</Form.Button>
+                </Form.Group>
+            </Form>
+
+            <Message
+                success
+                header="Form Completed"
+                content="You're signed up for Vaccination appointment alerts. We will send you an email when a vaccination slot is available."
+            />
+
+            <Message
+                error
+                header="Registration Failed"
+                content="The email you entered is already regsitered."
+            />
+        </Fragment>
+    );
 };
 
 export default reduxForm({
-  form: "profile"
+    form: "profile",
+    validate
 })(ProfileForm);
