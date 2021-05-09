34321ea14cd7535982bb470232803e3703e9666e
Prashanth Moorthy <chapparfellow@gmail.com>
2021-05-09 23:04:16 +0530

    Adding Yup for validation

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,42 +1,31 @@
 import React from "react";
 import { useFormik } from "formik";
 import { Divider, Input, Form, Button } from "semantic-ui-react";
+import * as Yup from "yup";
 
 export const ContactForm = () => {
-    const renderSelect = (field) => (
-        <Form.Select
-            label={field.label}
-            name={field.input.name}
-            onChange={(e, { value }) => field.input.onChange(value)}
-            options={field.options}
-            placeholder={field.placeholder}
-            value={field.input.value}
-        />
-    );
-
-    const validate = (values) => {
-        const errors = {};
-        if (!values.email) {
-            errors.email = "Required";
-        } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-            errors.email = "Invalid email address";
-        }
-
-        return errors;
-    };
-
     // Pass the useFormik() hook initial form values and a submit function that will
     // be called when the form is submitted
     const formik = useFormik({
         initialValues: {
             email: "",
             firstName: "",
-            lastName: "Default Last Name",
-            ageGroup: "Any",
+            lastName: "",
+            ageGroup: "18",
+            price: "ANY",
+            vaccine: "ANY",
         },
-        validate,
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .max(15, "Must be 15 characters or less")
+                .required("Required"),
+            lastName: Yup.string()
+                .max(20, "Must be 20 characters or less")
+                .required("Required"),
+            email: Yup.string()
+                .email("Invalid email address")
+                .required("Required"),
+        }),
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
         },
@@ -52,6 +41,13 @@ export const ContactForm = () => {
                     placeholder="First name"
                     value={formik.values.firstName}
                     onChange={formik.handleChange}
+                    error={
+                        formik.touched.firstName &&
+                        formik.errors.firstName && {
+                            content: formik.errors.firstName,
+                            pointing: "above",
+                        }
+                    }
                 />
 
                 <Form.Field
@@ -62,6 +58,13 @@ export const ContactForm = () => {
                     placeholder="Last name"
                     value={formik.values.lastName}
                     onChange={formik.handleChange}
+                    error={
+                        formik.touched.lastName &&
+                        formik.errors.lastName && {
+                            content: formik.errors.lastName,
+                            pointing: "above",
+                        }
+                    }
                 />
             </Form.Group>
 
@@ -72,10 +75,37 @@ export const ContactForm = () => {
                     label="Age Group"
                     name="ageGroup"
                     options={[
-                        { key: "m", text: "18 - 45 Years", value: "18" },
-                        { key: "f", text: "45+ Years", value: "45" },
+                        { key: "18", text: "18 - 45 Years", value: "18" },
+                        { key: "45", text: "45+ Years", value: "45" },
                     ]}
                     placeholder="Age Group"
+        defaultValue="18"
+                    required={true}
+                />
+
+                <Form.Select
+                    label="Price"
+                    name="price"
+                    options={[
+                        { key: "ANY", text: "Any", value: "ANY" },
+                        { key: "FREE", text: "Free", value: "FREE" },
+                        { key: "PAID", text: "Paid", value: "PAID" },
+                    ]}
+                    placeholder="Price"
+        defaultValue="ANY"
+                    required={true}
+                />
+
+                <Form.Select
+                    label="Vaccine Choice"
+                    name="vaccine"
+                    options={[
+                        { key: "ANY", text: "Any", value: "ANY" },
+                        { key: "COVI", text: "COVISHEILD", value: "COVI" },
+                        { key: "COVAX", text: "COVAXIN", value: "COVAX" },
+                    ]}
+                    placeholder="Select your Vaccine"
+        defaultValue="ANY"
                     required={true}
                 />
             </Form.Group>
@@ -94,7 +124,7 @@ export const ContactForm = () => {
                     formik.touched.email &&
                     formik.errors.email && {
                         content: formik.errors.email,
-                        pointing: "below",
+                        pointing: "above",
                     }
                 }
             />
